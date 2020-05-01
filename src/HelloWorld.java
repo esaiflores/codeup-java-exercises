b951c7f89ee698fb3bbaa6e8e28277ef6c1c3324
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-01 09:35:10 -0500

    cant assign characters or numbers to string or variable to nothing assigned

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,27 @@ public class HelloWorld {
         String myString = "This is my second time using java";
         System.out.println(myString);
 
+//      Change your code to assign a character value to myString. What do you notice?
+//      myString = (char) 't';
+//      System.out.println(myString);
+//      Doesn't work
+
+//      Change your code to assign the value 3.14159 to myString. What happens?
+//      myString = 3.14159;
+//      Doesn't work
+//
+//      Declare an long variable named myNumber, but do not assign anything to it.
+//      Next try to print out myNumber to the console. What happens?
+//      long myNumber;
+//      System.out.println(myNumber);
+//      Doesn't work
+
+
+
+
+
+
+
     }
 }
 
