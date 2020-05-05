ba8d7d5d619df65d185e1467532700be488d823a
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-05 10:22:47 -0500

    finished second output

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,7 +1,11 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String output = "We don't need no education";
-        System.out.println(output);
-        System.out.println(output.replace("education", "thought control"));
+
+//        String output = "We don't need no education";
+//        System.out.println(output);
+//        System.out.println(output.replace("education", "thought control"));
+
+        String message = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.print(message);
     }
 }
