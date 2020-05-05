34f701371003a89cf4863a22556d3bbad1c7805e
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-05 10:15:41 -0500

    finished first output

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String output = "We don't need no education";
+        System.out.println(output);
+        System.out.println(output.replace("education", "thought control"));
+    }
+}
