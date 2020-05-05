a1d6cc8a73f7319c7864f690d565aba71abae48d
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-05 10:24:16 -0500

    finished third message

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,7 +5,11 @@ public class StringExercise {
 //        System.out.println(output);
 //        System.out.println(output.replace("education", "thought control"));
 
-        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
+
+        String message = "In windows, the main drive is usually C:\\";
         System.out.print(message);
+
     }
 }
