b901256aa254da3ebea8d0828c3f5f35275f5846
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-05 10:04:31 -0500

    worked on exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -42,16 +42,18 @@ public class ControlFlowExercises {
 
 //        System.out.println(i);
 
-        for(int i = 1; i <= 100; i++){
-            if(i == 15){
-                System.out.println("FIZZBUZZ");
-            }else if(i % 3 == 0){
-                System.out.println("FIZZ");
-            }else if(i % 5 == 0){
-                System.out.println("BUZZ");
-            }else{
-                System.out.println(i);
-            }
-        }
+//        for(int i = 1; i <= 100; i++){
+//            if(i == 15){
+//                System.out.println("FIZZBUZZ");
+//            }else if(i % 3 == 0){
+//                System.out.println("FIZZ");
+//            }else if(i % 5 == 0){
+//                System.out.println("BUZZ");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
+
+        
     }
 }
